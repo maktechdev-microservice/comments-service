b95692fff37fa6733b8a51f91ef905573f0a1d27
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-11-02 23:46:05 +0100

    Changes url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post(route, async (req, res) => {
     const comments = commentsByPostId[req.params.id] || [];
     comments.push(newComment)
     commentsByPostId[req.params.id] = comments
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://events-clusterip-srv:4005/events", {
         type: "CommentCreated",
         data: {
             id,
