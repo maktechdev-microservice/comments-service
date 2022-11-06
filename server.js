d04bf3fc3ff02fff3d6f66dde09731b01e3c7f92
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-11-06 11:27:21 +0100

    Refactors the whole file and reformates it

--- server.js
+++ server.js
@@ -1,49 +1,72 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { randomBytes } from 'crypto'
+import express from "express";
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
 import cors from "cors";
-import axios from 'axios';
-
+import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
-const port = 4001
-const commentsByPostId = {}
-const route = "/posts/:id/comments"
+app.use(cors());
+const port = 4001;
+const route = "/posts/:id/comments";
+
+const commentsByPostId = {};
+
+const updateComment = async ({ id, status, postId, content }) => {
+  const comment = commentsByPostId[postId].find((c) => c.id === id);
+  comment.status = status;
+  await axios
+    .post("http://events-clusterip-srv:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    })
+    .catch((err) => {
+      console.log(`comment reports: ${err}`);
+    });
+};
 
 app.get(route, (req, res) => {
-    const comments = commentsByPostId[req.params.id] || []
-    res.send(comments);
+  const comments = commentsByPostId[req.params.id] || [];
+  res.send(comments);
 });
 
-app.post(route, async (req, res) => { 
-
-    const { content } = req.body;
-    const id = randomBytes(4).toString('hex');
-    const newComment = { id, content, status: 'pending' }
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push(newComment)
-    commentsByPostId[req.params.id] = comments
-    await axios.post("http://events-clusterip-srv:4005/events", {
-        type: "CommentCreated",
-        data: {
-            id,
-            content,
-            postId: req.params.id,
-            status: 'pending'
-        }
-    }).catch(err => {
-        console.log(`comment reports: ${err}`)
+app.post(route, async (req, res) => {
+  const { content } = req.body;
+  const id = randomBytes(4).toString("hex");
+  const newComment = { id, content, status: "pending" };
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push(newComment);
+  commentsByPostId[req.params.id] = comments;
+  await axios
+    .post("http://events-clusterip-srv:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id,
+        content,
+        postId: req.params.id,
+        status: "pending",
+      },
     })
-    res.status(201).send(newComment)
-})
+    .catch((err) => {
+      console.log(`comment reports: ${err}`);
+    });
+  res.status(201).send(newComment);
+});
 
-app.post("/events", (req, res) => {
-   console.log(`Comments service received "${req.body.type}" event`)
-    res.send({
-        status: "OK from comments"
-    })
-})
+app.post("/events", async (req, res) => {
+  console.log(`Comments service received "${req.body.type}" event`);
+  const { type, data } = req.body;
 
-app.listen(port, console.log(`Comments server listening on ${port}`))
+  if (type === "CommentModerated") {
+    updateComment(data);
+  }
+
+  res.send("OK from comments");
+});
+
+app.listen(port, console.log(`Comments server listening on ${port}`));
